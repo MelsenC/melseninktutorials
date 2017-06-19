e087594f58947dbe2357c6891dce8c0884c618c7
MelsenC <melsen.carlsburg@gmail.com>
2017-06-19 18:27:31 +0000

    moved the new lesson form to a modal from the instructor course page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   namespace :instructor do
     resources :lessons, only: [:update, :edit, :destroy]
     resources :sections, only: [:update] do
-      resources :lessons, only: [:new, :create]
+      resources :lessons, only: [:create]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
